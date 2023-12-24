2143b3d825cf1fd8a9c5d48208ab24081a83a9ec
RynoCODE <rohit.roychoudhury13@gmail.com>
2023-12-24 11:52:52 +0530

    Updated files

--- app/models.py
+++ app/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.utils.html import mark_safe
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from asgiref.sync import async_to_sync
+#from channels.layers import get_channel_layer
+
+import json
 
 # Create your models here.
+# Subscriber
+class Subscriber(models.Model):
+	user=models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+	mobile=models.CharField(max_length=20)
+	address=models.TextField()
+	img=models.ImageField(upload_to="subs/",null=True)
+
+	def __str__(self):
+		return str(self.user)
+
+	def image_tag(self):
+		if self.img:
+			return mark_safe('<img src="%s" width="80" />' % (self.img.url))
+		else:
+			return 'no-image'
+		
+@receiver(post_save,sender=User)
+def create_subscriber(sender,instance,created,**kwrags):
+	if created:
+		Subscriber.objects.create(user=instance)
+		
+# trainer
+class Trainer(models.Model):
+	full_name=models.CharField(max_length=100)
+	username=models.CharField(max_length=100,null=True)
+	pwd=models.CharField(max_length=50,null=True)
+	mobile=models.CharField(max_length=100)
+	address=models.TextField()
+	is_active=models.BooleanField(default=False)
+	detail=models.TextField()
+	img=models.ImageField(upload_to="trainers/")
+	salary=models.IntegerField(default=0)
+
+	facebook=models.CharField(max_length=200,null=True)
+	twitter=models.CharField(max_length=200,null=True)
+	pinterest=models.CharField(max_length=200,null=True)
+	youtube=models.CharField(max_length=200,null=True)
+
+	def __str__(self):
+		return str(self.full_name)
+
+	def image_tag(self):
+		if self.img:
+			return mark_safe('<img src="%s" width="80" />' % (self.img.url))
+		else:
+			return 'no-image'
+
+
+class AssignSubscriber(models.Model):
+	user=models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+	trainer=models.ForeignKey(Trainer, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.user)
+
--- app/urls.py
+++ app/urls.py
@@ -1,2 +1,5 @@
 from django.urls import path
-
+from . import views
+urlpattern = [
+    path('', views.home)
+]
